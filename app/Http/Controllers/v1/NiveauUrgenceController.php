c874da765d5d624ffe21e8c8430da4417a7738b8
Berthold FEUJO <feujio@medicasure.com>
2022-11-24 16:58:08 +0100

    Recuperation des niveau d'urgence

--- app/Http/Controllers/v1/NiveauUrgenceController.php
+++ app/Http/Controllers/v1/NiveauUrgenceController.php
@@ -22,10 +22,10 @@ class NiveauUrgenceController extends Controller
 
     }
 
-    public function show($niveau_urgences){
+    public function show($niveau_urgence){
 
-        $niveau_urgences = NiveauUrgence::findOrFail($niveau_urgences)->makeHidden(['created_at', 'updated_at', 'deleted_at']);
-        return $this->successResponse($niveau_urgences);
+        $niveau_urgence = NiveauUrgence::findOrFail($niveau_urgence)->makeHidden(['created_at', 'updated_at', 'deleted_at']);
+        return $this->successResponse($niveau_urgence);
 
     }
 
