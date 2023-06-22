a7c2479d4c43769059dadbc36a46b6a690ebbc3e
mouafo <mouafo@medicasure.com>
2023-06-22 10:10:58 +0100

    niveau d'urgence langue en

---
+++ database/migrations/2023_06_22_090421_update_niveau_urgence_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('niveau_urgences', function (Blueprint $table) {
+
+            $table->text('description_en')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('niveau_urgences', function (Blueprint $table) {
+            $table->dropColumn('description_en');
+        });
+    }
+};
--- database/seeders/NiveauUrgenceSeeder.php
+++ database/seeders/NiveauUrgenceSeeder.php
@@ -16,14 +16,14 @@ class NiveauUrgenceSeeder extends Seeder
     public function run()
     {
         $niveaux = [
-            ['valeur' => 1, 'description' => "Urgences sans risque particulier, cette situation ne nécessite pas de soins d'urgence, la prise en charge peut être différée"],
-            ['valeur' => 2, 'description' => "Urgences sans risque particulier, cette situation ne nécessite pas de soins d'urgence, la prise en charge peut être différée"],
-            ['valeur' => 3, 'description' => "Urgence modérée, la vie du patient n'est pas en danger, son état est stable mais pourrait nécessiter des investigations complémentaires"],
-            ['valeur' => 4, 'description' => "Urgence majeure avec risque vital potentiel. La situation du patient pourrait s'aggraver rapidement"],
-            ['valeur' => 5, 'description' => "Urgence vitale, la prise en charge médicale est immédiate"],
+            ['valeur' => 1, 'description' => "Urgences sans risque particulier, cette situation ne nécessite pas de soins d'urgence, la prise en charge peut être différée", 'description_en' => "Emergencies without particular risk, this situation does not require emergency care, management can be postponed"],
+            ['valeur' => 2, 'description' => "Urgences sans risque particulier, cette situation ne nécessite pas de soins d'urgence, la prise en charge peut être différée", 'description_en' => "Emergencies without particular risk, this situation does not require emergency care, management can be postponed"],
+            ['valeur' => 3, 'description' => "Urgence modérée, la vie du patient n'est pas en danger, son état est stable mais pourrait nécessiter des investigations complémentaires", 'description_en' => "Moderate emergency, the patient's life is not in danger, his condition is stable but may require further investigation"],
+            ['valeur' => 4, 'description' => "Urgence majeure avec risque vital potentiel. La situation du patient pourrait s'aggraver rapidement", 'description_en' => "Major emergency with potential life-threatening risk. The patient's situation could worsen rapidly"],
+            ['valeur' => 5, 'description' => "Urgence vitale, la prise en charge médicale est immédiate", 'description_en' => "Vital emergency, medical care is immediate"],
         ];
 
-        foreach($niveaux as $niveau){
+        foreach ($niveaux as $niveau) {
             NiveauUrgence::create($niveau);
         }
     }
